2518403c2ac9e4e95ece1bfd2b4d2175fe4228f5
StocktonManges <stocktonmanges@gmail.com>
2024-03-20 08:35:21 -0600

    initial commit

---
+++ components/CategoryGridTile.js
@@ -0,0 +1,15 @@
+import { Pressable, StyleSheet, View, Text } from "react-native";
+
+export default function CategoryGridTile({ title, color }) {
+  return (
+    <View>
+      <Pressable>
+        <View>
+          <Text>{title}</Text>
+        </View>
+      </Pressable>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
---
+++ screens/CategoriesScreen.js
@@ -0,0 +1,21 @@
+import { FlatList, View } from "react-native";
+import { CATEGORIES } from "../data/dummy-data";
+import CategoryGridTile from "../components/CategoryGridTile";
+
+const renderCategoryItem = (itemData) => {
+  return (
+    <CategoryGridTile title={itemData.item.title} color={itemData.item.color} />
+  );
+};
+
+export default function CategoriesScreen() {
+  return (
+    <View>
+      <FlatList
+        data={CATEGORIES}
+        keyExtractor={(item) => item.id}
+        renderItem={renderCategoryItem}
+      />
+    </View>
+  );
+}
